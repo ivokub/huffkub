c4441558c10220a7c94bb37999e0369ce56d6282
Ivo Kubjas <ivo.kubjas@eesti.ee>
2012-10-25 15:46:43 +0300

    Modified tests
    Added true oneletter test (not including newline), which fails atm.
    Modified output.
    For big files: dd if=/dev/urandom of=plain/bigfile bs=1k count=10k

--- tests/testscript.sh
+++ tests/testscript.sh
@@ -6,18 +6,18 @@ echo "Testing packer:"
 rm packed/*
 rm unpacked/*
 for file in `ls plain/`; do
-	echo "	Packing $file:"
-	../huffkub -c -f plain/$file -d packed/$file && echo "		Success"
+	echo "  File $file:"
+	echo -n "   Packing: "
+	../huffkub -c -f plain/$file -d packed/$file && echo "Success"
+	echo -n "   Unpacking: "
+	../huffkub -x -f packed/$file -d unpacked/$file && echo "Success"
+	echo -n "   Comparing: "
+	cmp unpacked/$file plain/$file && echo "Success"
+	SIZE1=$(wc -c plain/$file | cut -d" " -f1)
+	SIZE2=$(wc -c packed/$file | cut -d" " -f1)
+	echo "   Size before: $SIZE1, size after: $SIZE2"
 done
 
-for file in `ls packed/`; do
-	echo "	Unpacking $file:"
-        ../huffkub -x -f packed/$file -d unpacked/$file && echo "		Success"
-done
-
-for file in `ls plain/`; do
-	echo "	Comparing $file:"
-        cmp unpacked/$file plain/$file && echo "		Success:"
-done
-
+rm packed/*
+rm unpacked/*
 echo "All tests done"
